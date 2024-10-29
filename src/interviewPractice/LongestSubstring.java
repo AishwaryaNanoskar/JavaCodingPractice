16a71a1d7c1b2e92b2ffe9c4cb0cc05cb3bec928
AishwaryaNanoskar <aishwaryananoskar30@gmail.com>
2024-10-29 10:58:36 +0530

    dummmy

--- src/interviewPractice/LongestSubstring.java
+++ src/interviewPractice/LongestSubstring.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class LongestSubstring {
 	public static void main(String[] args) {
-		String str = "abcabcde";
+		String str = "abcabcde";  
 
 		List<Character> data = new ArrayList<>();
 		List<Integer> dataCount = new ArrayList<>();
